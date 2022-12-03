c1ad6f92ef6e1204481aff470961c5aae1ebdc64
Tubby <git@tubby.nl>
2022-12-03 10:12:53 +0100

    added sumCalories and testing indivdual elves colories

--- src/main/java/nl/tubby/aoc/Elf.java
+++ src/main/java/nl/tubby/aoc/Elf.java
@@ -1,7 +1,12 @@
 package nl.tubby.aoc;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public record Elf(int nr,List<FoodItem> inventory){}
+public record Elf(int nr,List<FoodItem> inventory){
+    int sumCalories() {
+        return inventory().stream().collect(Collectors.summingInt(FoodItem::calories));
+    }
+}
 
 record FoodItem(int calories){}
--- src/test/java/nl/tubby/aoc/ElfFactoryTest.java
+++ src/test/java/nl/tubby/aoc/ElfFactoryTest.java
@@ -2,6 +2,7 @@ package nl.tubby.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,5 +17,22 @@ public class ElfFactoryTest {
         List<Elf> elves = elfFactory.build();
 
         assertEquals(5,elves.size());
+        assertEquals(1,elves.get(0).nr());
+        assertEquals(1000,elves.get(0).sumCalories());
+
+        assertEquals(2,elves.get(1).nr());
+        assertEquals(2000,elves.get(1).sumCalories());
+    }
+
+    @Test
+    void findTheBest() {
+        FoodItemSlurper slurper = new FoodItemSlurper("src/test/resources","example.txt");
+        ElfFactory elfFactory = new ElfFactory(slurper);
+
+        Elf best = elfFactory.build().stream()
+                .sorted(Comparator.comparing(Elf::sumCalories))
+                .findFirst().get();
+
+        assertEquals(4,best.nr());
     }
 }
