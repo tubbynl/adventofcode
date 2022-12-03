f65da616c7647b1329683209f049acde45b29691
Tubby <git@tubby.nl>
2022-12-03 09:05:03 +0100

    renamed to FoodItemSlurperTest

--- src/test/java/nl/tubby/aoc/SluperTest.java
+++ src/test/java/nl/tubby/aoc/SluperTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ElfTest {
+class FoodItemSlurperTest {
 
     @Test
     void findElveWithMostFoodItems() {
