c201294807b1f9490b82d8059b582a155485442a
Tubby <git@tubby.nl>
2022-12-03 09:41:03 +0100

    arg; Optional.empty cannot be found in a list

--- src/main/java/nl/tubby/aoc/ElfFactory.java
+++ src/main/java/nl/tubby/aoc/ElfFactory.java
@@ -1,9 +1,12 @@
 package nl.tubby.aoc;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ElfFactory {
+    private static final Optional<FoodItem> EMPTY = Optional.empty();
     private final FoodItemSlurper slurper;
 
     public ElfFactory(FoodItemSlurper slurper) {
@@ -11,6 +14,17 @@ public class ElfFactory {
     }
 
     public List<Elf> build() {
-        return Collections.emptyList();
+        List<Optional<FoodItem>> foodItems = this.slurper.slurp().collect(Collectors.toList());
+
+        int index = 0;
+        int elfNr = 1;
+        List<Elf> elves = new ArrayList<>();
+        while(foodItems.contains(Optional.empty())) {
+            int nextIndex = foodItems.indexOf(Optional.empty());
+            elves.add(new Elf(elfNr,foodItems.subList(index,nextIndex).stream().map(Optional::get).collect(Collectors.toList())));
+            elfNr++;
+            index = nextIndex;
+        }
+        return elves;
     }
 }
