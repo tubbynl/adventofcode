26217e496b1b30ed15ccf2e4ee2fe5badba8aed3
Tubby <git@tubby.nl>
2022-12-03 15:03:51 +0100

    fixed outcome calc

--- src/main/java/nl/tubby/aoc/RockPaperScissorsRound.java
+++ src/main/java/nl/tubby/aoc/RockPaperScissorsRound.java
@@ -1,12 +1,22 @@
 package nl.tubby.aoc;
 
+import static nl.tubby.aoc.RockPaperScissors.*;
 record RockPaperScissorsRound(RockPaperScissors opponent, RockPaperScissors me) {
 
     RoundState outcome() {
-        if(opponent().equals(me)) {
+        if(me.equals(opponent)) {
             return RoundState.tie;
         }
-        return RoundState.win;
+        if(me.equals(rock) && opponent.equals(scissors)) {
+            return RoundState.win;
+        }
+        if(me.equals(scissors) && opponent.equals(paper)) {
+            return RoundState.win;
+        }
+        if(me.equals(paper) && opponent.equals(rock)) {
+            return RoundState.win;
+        }
+        return RoundState.loss;
     }
 }
 
