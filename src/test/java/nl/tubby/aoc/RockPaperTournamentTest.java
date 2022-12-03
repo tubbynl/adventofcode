4b9aec3e4f4086d95f90c36508df267b33a7ef0c
Tubby <git@tubby.nl>
2022-12-03 15:24:52 +0100

    prepare test for my input ParameterizedTest ftw \o/

--- src/test/java/nl/tubby/aoc/RockPaperTournamentTest.java
+++ src/test/java/nl/tubby/aoc/RockPaperTournamentTest.java
@@ -1,6 +1,9 @@
 package nl.tubby.aoc;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Collectors;
 
@@ -8,14 +11,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RockPaperTournamentTest {
 
-    @Test
-    void tournament() {
-        var slurper = new RockPaperScissorsRoundSlurper("src/test/resources","puzzle-example-day2.txt");
+    @ParameterizedTest
+    @CsvSource({
+            "puzzle-example-day2.txt,15"
+    })
+    void tournament(String filename, int expectedScore) {
+        var slurper = new RockPaperScissorsRoundSlurper("src/test/resources",filename);
 
-        var rounds = slurper.slurp().collect(Collectors.toList());
+        var rounds = slurper.slurp();
 
-        var totalScore = rounds.stream().collect(Collectors.summingInt(RockPaperScissorsRound::calculateScore));
+        var totalScore = rounds.collect(Collectors.summingInt(RockPaperScissorsRound::calculateScore));
 
-        assertEquals(15,totalScore);
+        assertEquals(expectedScore,totalScore);
     }
 }
