47f877746bf89380b4a6fa3cd4dd9db8f6f6d60d
Tubby <git@tubby.nl>
2022-12-03 15:42:35 +0100

    added aliases/parse to RoundState

--- src/main/java/nl/tubby/aoc/RoundState.java
+++ src/main/java/nl/tubby/aoc/RoundState.java
@@ -1,20 +1,36 @@
 package nl.tubby.aoc;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static nl.tubby.aoc.RockPaperScissors.*;
 
 public enum RoundState {
-    loss(0),
-    tie(3),
-    win(6);
+    loss(0, "X"),
+    tie(3, "Y"),
+    win(6, "Z");
 
+    private static final Map<String,RoundState> MAP = Stream.of(values())
+            .collect(Collectors.toMap(RoundState::getAlias, Function.identity()));
     private final int score;
+    private final String alias;
 
-    RoundState(int score) {
+    RoundState(int score, String alias) {
         this.score = score;
+        this.alias = alias;
     }
+
+
 
     public int getScore() {
         return score;
+    }
+
+    public String getAlias() {
+        return alias;
     }
 
     static RoundState determine(RockPaperScissors opponent, RockPaperScissors me) {
@@ -31,5 +47,10 @@ public enum RoundState {
             return win;
         }
         return loss;
+    }
+
+    static Optional<RoundState> parse(String value) {
+        return Optional.ofNullable(value)
+                .map(MAP::get);
     }
 }
