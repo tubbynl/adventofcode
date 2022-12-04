d163ad2d7fbe3cda22e1651994195a318705abe1
Tubby <git@tubby.nl>
2022-12-04 16:33:01 +0100

    as ParameterizedTest for testing second Rucksack

--- src/test/java/nl/tubby/aoc/RucksackTest.java
+++ src/test/java/nl/tubby/aoc/RucksackTest.java
@@ -1,16 +1,21 @@
 package nl.tubby.aoc;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RucksackTest {
 
-    @Test
-    void parse() {
-        var rucksack = Rucksack.parse("vJrwpWtwJgWrhcsFMMfFFhFp");
+    @ParameterizedTest
+    @CsvSource({
+            "vJrwpWtwJgWrhcsFMMfFFhFp,vJrwpWtwJgWr,hcsFMMfFFhFp"
+    })
+    void parse(String line,String first,String second) {
+        var rucksack = Rucksack.parse(line);
 
-        assertEquals("vJrwpWtwJgWr",rucksack.compartment1());
-        assertEquals("hcsFMMfFFhFp",rucksack.compartment2());
+        assertEquals(first,rucksack.compartment1());
+        assertEquals(second,rucksack.compartment2());
     }
 }
