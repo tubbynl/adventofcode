4d56becb123f9b3a4533e168ed23d46aabb5f272
Tubby <git@tubby.nl>
2022-12-08 17:18:34 +0100

    test on visibility more usable in feedback

--- src/test/java/nl/tubby/aoc/MatrixTest.java
+++ src/test/java/nl/tubby/aoc/MatrixTest.java
@@ -3,35 +3,36 @@ package nl.tubby.aoc;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatrixTest {
+    Matrix sut = Matrix.slurp(Path.of("puzzle-example-day8.txt"));
 
     @Test
     void rowAndColum() {
-        var matrix = Matrix.slurp(Path.of("puzzle-example-day8.txt"));
-
-        assertEquals(List.of(3,0,3,7,3),matrix.row(0).toList());
-        assertEquals(List.of(3,5,3,5,3),matrix.col(2).toList());
-        assertEquals(4,matrix.value(3,3));
+        assertEquals(List.of(3,0,3,7,3),sut.row(0).toList());
+        assertEquals(List.of(3,5,3,5,3),sut.col(2).toList());
+        assertEquals(4,sut.value(3,3));
     }
 
-    @Test
-    void isVisible() {
-        var matrix = Matrix.slurp(Path.of("puzzle-example-day8.txt"));
-
-        assertEquals(1,matrix.isVisible(1,1));// the top-left 5 is visible from the left
-        assertEquals(2,matrix.isVisible(1,2));// the top-middle 5 is visible from the top
-        assertEquals(0,matrix.isVisible(1,3));// the top-right 1 is not visible
-        assertEquals(1,matrix.isVisible(2,1));// the left-middle 5 is visible
-        assertEquals(0,matrix.isVisible(2,2));// the center 3 is not visible
-        assertEquals(1,matrix.isVisible(2,2));// right-middle 3 is visible
-        assertEquals(1,matrix.isVisible(3,2));// the bottom row, the middle 5 is visible
-        assertEquals(0,matrix.isVisible(3,1));// the bottom row, 3 is not visible
-        assertEquals(0,matrix.isVisible(3,3));// the bottom row, 4 is not visible
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,1,the top-left 5 is visible from the left",
+            "1,2,2,the top-middle 5 is visible from the top",
+            "1,3,0,the the top-right 1 is not visible",
+            "2,1,1,the the left-middle 5 is visible",
+            "2,2,0,the center 3 is not visible",
+            "2,3,1,right-middle 3 is visible",
+            "3,2,1,the bottom row, the middle 5 is visible",
+            "3,1,0,the bottom row, 3 is not visible",
+            "3,3,0,the bottom row, 4 is not visible",
+    })
+    void isVisible(int row,int col,int expectedVisibility,String message) {
+        assertEquals(expectedVisibility,sut.isVisible(row,col),message);
     }
 
     @ParameterizedTest
