1cd3641802ec30bf1056a1587f082ab89dabf1c2
Tubby <git@tubby.nl>
2022-12-08 17:58:27 +0100

    fixed asserts on viewable from the right

--- src/test/java/nl/tubby/aoc/MatrixTest.java
+++ src/test/java/nl/tubby/aoc/MatrixTest.java
@@ -3,7 +3,6 @@ package nl.tubby.aoc;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -24,12 +23,12 @@ class MatrixTest {
             "1,1,1,the top-left 5 is visible from the left",
             "1,2,2,the top-middle 5 is visible from the top",
             "1,3,0,the the top-right 1 is not visible",
-            "2,1,1,the the left-middle 5 is visible",
+            "2,1,3,the the left-middle 5 is visible from the right",
             "2,2,0,the center 3 is not visible",
-            "2,3,1,right-middle 3 is visible",
+            "2,3,3,right-middle 3 is visible from the right",
             "3,2,1,the bottom row, the middle 5 is visible",
-            "3,1,0,the bottom row, 3 is not visible",
-            "3,3,0,the bottom row, 4 is not visible",
+            "3,1,0,the bottom row 3 is not visible",
+            "3,3,0,the bottom row 4 is not visible",
     })
     void isVisible(int row,int col,int expectedVisibility,String message) {
         assertEquals(expectedVisibility,sut.isVisible(row,col),message);
