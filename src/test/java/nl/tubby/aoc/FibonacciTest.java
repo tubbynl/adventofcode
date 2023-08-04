e1aa3e66d4e0ab3b6f149da68d85c093773f975e
Tubby <git@tubby.nl>
2023-08-04 20:57:39 +0200

    test with expectedValues
    https://nl.wikipedia.org/wiki/Rij_van_Fibonacci

--- src/test/java/nl/tubby/aoc/FibonacciTest.java
+++ src/test/java/nl/tubby/aoc/FibonacciTest.java
@@ -2,21 +2,21 @@ package nl.tubby.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciTest {
-
     @Test
     void next() {
         var fibo = new Fibonacci();
 
-        for (int i = 0; i < 10; i++) {
-            var next = fibo.next();
-            System.err.println(i + " next: " + next);
-        }
-
-        System.err.println(fibo);
+        Stream.of(1, 2, 3, 5, 8, 13, 21, 34, 55, 89)
+            .forEach(expectedValue -> {
+                assertEquals(expectedValue,fibo.next());
+            });
     }
 }
