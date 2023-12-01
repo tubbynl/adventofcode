a3f83555f6a2f3a93a549922b3142a4b6098cdc0
Tubby <git@tubby.nl>
2023-12-01 13:33:32 +0100

    implemented; added test

--- src/main/java/nl/tubby/aoc23/Day1Calibator.java
+++ src/main/java/nl/tubby/aoc23/Day1Calibator.java
@@ -1,8 +1,14 @@
 package nl.tubby.aoc23;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Day1Calibator {
 
     static Integer parse(String input) {
-        return 1;
+        var onlyDigits = StringUtils.getDigits(input);
+
+        var first = StringUtils.substring(onlyDigits,0,1);
+        var last = StringUtils.substring(onlyDigits,-1);
+        return Integer.parseInt(first+last);
     }
 }
--- src/test/java/nl/tubby/aoc23/Day1CalibatorTest.java
+++ src/test/java/nl/tubby/aoc23/Day1CalibatorTest.java
@@ -14,9 +14,21 @@ class Day1CalibatorTest {
             "aoc-2023-day1-example.txt,142",
             "puzzle-input-day4.txt,511"// <-- solution part 1
     })
-    void extract(String file,int expectedCalibrationSum) {
+    void extractSum(String file,int expectedCalibrationSum) {
         var slurper = new Slurper<>(Day1Calibator::parse);
 
         assertEquals(expectedCalibrationSum,slurper.sum(Path.of(file),Integer::intValue));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1abc2,12",
+            "pqr3stu8vwx,38",
+            "a1b2c3d4e5f,15",
+            "treb7uchet,77"
+    })
+    void extract(String input,int expectedCalibration) {
+
+        assertEquals(expectedCalibration,Day1Calibator.parse(input));
     }
 }
