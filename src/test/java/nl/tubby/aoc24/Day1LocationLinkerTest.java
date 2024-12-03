4ca0d1a781c18d27a73eedf14f1cf54cf205412f
Tubby <git@tubby.nl>
2024-12-03 14:57:16 +0100

    as parameterized test

--- src/test/java/nl/tubby/aoc24/Day1LocationLinkerTest.java
+++ src/test/java/nl/tubby/aoc24/Day1LocationLinkerTest.java
@@ -3,6 +3,9 @@ package nl.tubby.aoc24;
 import nl.tubby.Resource;
 import nl.tubby.Slurper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -10,11 +13,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day1LocationLinkerTest {
 
-    @Test
-    void totalDistance() {
+    @ParameterizedTest
+    @CsvSource({
+            "day1-example.txt,11"
+    })
+    void totalDistance(Resource file,int expected) {
         var slurper = new Slurper<>(Day1LocationLinker::parse);
 
-        var result = slurper.list(Resource.of("day1-example.txt"));
+        var result = slurper.list(file);
         var right = result.stream().mapToInt(Day1LocationLinker.Pair::right).sorted().iterator();
 
         var totalDistance = result.stream()
@@ -24,6 +30,6 @@ class Day1LocationLinkerTest {
                 .mapToInt(Day1LocationLinker.Pair::distance)
                 .sum();
 
-        assertEquals(11,totalDistance);
+        assertEquals(expected,totalDistance);
     }
 }
