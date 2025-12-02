ad9fbc5756acccff0be5a03bb3263a096dedca3a
Tubby <git@tubby.nl>
2025-12-02 21:34:15 +0100

    extraced Predicate for filtering step1 and step2; solution step2 :)

--- src/main/java/nl/tubby/aoc25/Day2GiftShop.java
+++ src/main/java/nl/tubby/aoc25/Day2GiftShop.java
@@ -1,6 +1,8 @@
 package nl.tubby.aoc25;
 
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Day2GiftShop {
@@ -11,19 +13,36 @@ public class Day2GiftShop {
     }
 
 
-    record IdRange(int start, int end) {
+    record IdRange(long start, long end) {
         static IdRange parse(String pair) {
             var splitted = pair.split("-",2);
-            return new IdRange(Integer.parseInt(splitted[0]),Integer.parseInt(splitted[1]));
+            return new IdRange(Long.parseLong(splitted[0]),Long.parseLong(splitted[1]));
         }
 
-        Stream<Integer> invalidIds() {
-            return IntStream.range(this.start,this.end+1)
+        Stream<Long> invalidIds(Predicate<String> filter) {
+            return LongStream.range(this.start,this.end+1)
                     .boxed()
                     .map(Object::toString)
-                    .filter(id -> id.length()%2==0)
-                    .filter(id -> id.substring(0,id.length()/2).equals(id.substring(id.length()/2)))
-                    .map(Integer::parseInt);
+                    .filter(filter)
+                    .map(Long::parseLong);
+        }
+
+        static boolean idCheck1(String id) {
+            return id.length()%2==0 && id.substring(0,id.length()/2).equals(id.substring(id.length()/2));
+        }
+
+        static boolean idCheck2(String id) {
+            int half = id.length()/2;
+            for(int i=1;i<=half;i++) {
+                String partial = id.substring(0,i);
+                if(id.length()%partial.length()==0) {
+                    int repeat = id.length()/partial.length();
+                    if(partial.repeat(repeat).equals(id)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
--- src/test/java/nl/tubby/aoc25/Day2GiftShopTest.java
+++ src/test/java/nl/tubby/aoc25/Day2GiftShopTest.java
@@ -15,9 +15,10 @@ class Day2GiftShopTest {
 
     @ParameterizedTest
     @CsvSource({
-            "day2-example.txt,11,8,1227775554"
+            "day2-example.txt,11,8,1227775554",
+            "day2-input1.txt,33,734,19605500130"
     })
-    void parse(Resource file, int rangeCount, int invalidIdCount, int sum) {
+    void parsePart1(Resource file, int rangeCount, int invalidIdCount, long sum) {
         var slurper = new Slurper<>(Day2GiftShop::parse);
 
         var idRanges = slurper.slurp(file)
@@ -26,10 +27,35 @@ class Day2GiftShopTest {
 
         assertEquals(rangeCount,idRanges.size());
 
-        var invalidIds = idRanges.stream().flatMap(Day2GiftShop.IdRange::invalidIds).toList();
+        var invalidIds = idRanges.stream()
+                .flatMap(range -> range.invalidIds(Day2GiftShop.IdRange::idCheck1))
+                .toList();
 
         assertEquals(invalidIdCount,invalidIds.size());
 
-        assertEquals(sum,invalidIds.stream().mapToInt(Integer::intValue).sum());
+        assertEquals(sum,invalidIds.stream().mapToLong(Long::longValue).sum());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "day2-example.txt,11,13,4174379265",
+            "day2-input1.txt,33,802,36862281418"
+    })
+    void parsePart2(Resource file, int rangeCount, int invalidIdCount, long sum) {
+        var slurper = new Slurper<>(Day2GiftShop::parse);
+
+        var idRanges = slurper.slurp(file)
+                .flatMap(Function.identity())
+                .toList();
+
+        assertEquals(rangeCount,idRanges.size());
+
+        var invalidIds = idRanges.stream()
+                .flatMap(range -> range.invalidIds(Day2GiftShop.IdRange::idCheck2))
+                .toList();
+
+        assertEquals(invalidIdCount,invalidIds.size());
+
+        assertEquals(sum,invalidIds.stream().mapToLong(Long::longValue).sum());
     }
 }
